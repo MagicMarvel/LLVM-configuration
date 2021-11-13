862fd6acd79bf92e59af7135e597b9b3a1bc7db9
MagicMarvel <magicmarvel@163.com>
2021-11-13 22:04:48 +0800

    V 0.0.1

---
+++ main.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <string>
+#include <vector>
+using namespace std;
+int main() {
+
+    auto i = 1;
+    double b = i;
+    string s;
+
+    if (1 < 2) {
+        cout << "1<2";
+    }
+
+    return 0;
+}
